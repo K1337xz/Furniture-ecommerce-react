cfb4a97b2351f17cf2cbfc81cd4266391e5a9aa4
Koporson <108631492+K1337xz@users.noreply.github.com>
2023-10-20 12:49:31 +0200

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Homepage from "./pages/homepage/Homepage.jsx";
 import "./App.css";
-
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
 			<div>123</div>
