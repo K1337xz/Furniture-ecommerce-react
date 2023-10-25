b01c9797f424b66f2224c3c83552acb4e5845312
Koporson <108631492+K1337xz@users.noreply.github.com>
2023-10-25 21:39:30 +0200

    update

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/furniture-logos_white.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import "./footer.scss";
 
 export default function Footer() {
@@ -12,18 +14,35 @@ export default function Footer() {
 					</Link>
 				</div>
 				<div className="footer__rightSide">
-					<ul className="footer__nav">
-						<li>
-							Company
-							<ul className="footer__nav">
-								<li>About Us</li>
-								<li>Careers</li>
-								<li>FAQs</li>
-								<li>Teams</li>
-								<li>Contact Us</li>
-							</ul>
-						</li>
-					</ul>
+					<div className="footer__nav">
+						<div className="footer__navContent">
+							<h4>Company</h4>
+							<div className="footer__items">
+								<Link>About Us</Link>
+								<Link>Careers</Link>
+								<Link>FAQs</Link>
+								<Link>Teams</Link>
+								<Link>Contact Us</Link>
+							</div>
+						</div>
+						<div className="footer__navContent">
+							<h4>Products</h4>
+							<div className="footer__items">
+								<Link>Tables</Link>
+								<Link>Shelves and Cabinets</Link>
+								<Link>Desks</Link>
+								<Link>Beds and Bedside tables</Link>
+								<Link>Chairs</Link>
+								<Link>Sofas</Link>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="footer__location">
+					<div className="footer__locationValues">
+						<FontAwesomeIcon icon={faLocationDot} />{" "}
+						<Link to="#">loremipsum@loremipsum.com</Link>
+					</div>
 				</div>
 			</div>
 		</footer>
