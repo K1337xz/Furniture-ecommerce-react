6f7369a72c85ef0aa4a6a9b5e3a8726fdbcf6ec4
Koporson <108631492+K1337xz@users.noreply.github.com>
2024-01-10 20:36:47 +0100

    Update cron-task.js

--- server/furniture/config/cron-task.js
+++ server/furniture/config/cron-task.js
@@ -4,15 +4,13 @@ module.exports = {
       console.log("hwj");
       try {
         console.log("work after 14min" + new Date());
-        console.log("huh");
-        console.log("x");
       } catch (err) {
         console.error(err);
       }
     },
     options: {
       // Every 14min
-      rule: "*/14 * * * *",
+      rule: "*/5 * * * *",
     },
   },
 };
