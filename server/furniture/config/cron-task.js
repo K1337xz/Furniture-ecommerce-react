c6b6de8f15c3341714c23861c6a13d024c5f5c62
Koporson <108631492+K1337xz@users.noreply.github.com>
2024-01-10 19:06:36 +0100

    Update cron-task.js

--- server/furniture/config/cron-task.js
+++ server/furniture/config/cron-task.js
@@ -3,13 +3,14 @@ module.exports = {
     task: async ({ strapi }) => {
       console.log("cd" + new Date());
       try {
+        console.log("cd" + new Date());
       } catch (err) {
         console.error(err);
       }
     },
     options: {
       // Every minute
-      rule: "/10 * * * * *",
+      rule: "0/10 * * * * *",
     },
   },
 };
