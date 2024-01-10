a695d9e19f98e7eacd0c7db138fbd9772aa0d450
Koporson <108631492+K1337xz@users.noreply.github.com>
2024-01-10 18:35:22 +0100

    Update server.js

--- server/furniture/config/server.js
+++ server/furniture/config/server.js
@@ -1,11 +1,11 @@
-const productsCount = require("./cron-task");
+const cronTasks = require("./cron-task");
 
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   cron: {
     enabled: true,
-    tasks: productsCount,
+    tasks: cronTasks,
   },
   app: {
     keys: env.array("APP_KEYS"),
