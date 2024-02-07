2c80e2898e57d74e3966bf2c65e8785579ea95ba
Koporson <108631492+K1337xz@users.noreply.github.com>
2024-02-07 17:54:32 +0100

    Update Orders.js

---
+++ client/src/components/orders/Orders.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import "./orders.scss";
+import { useEffect } from "react";
+export default function Orders() {
+	useEffect(() => {
+		const fetchData = async () => {};
+	});
+	return <div className="order"></div>;
+}
