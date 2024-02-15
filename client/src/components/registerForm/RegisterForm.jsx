623f866a58c8bee676ac874e83fd4875a144df18
Koporson <108631492+K1337xz@users.noreply.github.com>
2024-02-15 19:30:57 +0100

    update

--- client/src/components/registerForm/RegisterForm.jsx
+++ client/src/components/registerForm/RegisterForm.jsx
@@ -2,7 +2,6 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { ref } from "yup";
-
 import "./registerForm.scss";
 import { Link } from "react-router-dom";
 
