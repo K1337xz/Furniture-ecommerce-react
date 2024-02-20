c57afe3acd14186af73d0db524c40605e931b34b
Koporson <108631492+K1337xz@users.noreply.github.com>
2024-02-20 16:23:50 +0100

    Update plugins.js

--- server/furniture/config/plugins.js
+++ server/furniture/config/plugins.js
@@ -19,9 +19,9 @@ module.exports = ({ env }) => ({
     config: {
       provider: "mailgun",
       providerOptions: {
-        key: env("MAILGUN_API_KEY"), // Required
-        domain: env("MAILGUN_DOMAIN"), // Required
-        url: env("MAILGUN_URL", "https://api.mailgun.net"), //Optional. If domain region is Europe use 'https://api.eu.mailgun.net'
+        key: env("MAILGUN_API_KEY"),
+        domain: env("MAILGUN_DOMAIN"),
+        url: env("MAILGUN_URL", "https://api.mailgun.net"),
       },
       settings: {
         defaultFrom: "myemail@protonmail.com",
