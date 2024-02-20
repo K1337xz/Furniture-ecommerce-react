ec618af1c18ff88c06c6973bcadf30dccf739bc5
Koporson <108631492+K1337xz@users.noreply.github.com>
2024-02-20 15:10:54 +0100

    Update cron-task.js

--- server/furniture/config/cron-task.js
+++ server/furniture/config/cron-task.js
@@ -1,17 +1,18 @@
 module.exports = {
   productsCount: {
     task: async ({ strapi }) => {
+      console.log("?????");
       try {
-        await strapi.stop();
-        console.log("xddddd");
-        await strapi.start();
+        const products = await strapi.query("products").find();
+        const productsCount = products.length;
+        console.log(productsCount);
       } catch (err) {
         console.error(err);
       }
     },
     options: {
       // Every 14min
-      rule: "*/10 * * * *",
+      rule: "*/1 * * * *",
     },
   },
 };
